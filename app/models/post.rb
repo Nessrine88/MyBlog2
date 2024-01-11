b31aeccecf614af975d41a57b7c5f9093e9b54b9
unknown <macherki.nessrine@yahoo.com>
2024-01-11 18:28:16 +0100

    update post model file

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,6 @@ class Post < ApplicationRecord
   private
 
   def update_post_counters
-    user.update(posts_counter: user.posts.count)
+    author.update(posts_counter: author.posts.count)
   end
 end
