f46768b7aa4b86d8c05ed0caffcdec8856a4d22f
unknown <macherki.nessrine@yahoo.com>
2024-01-12 11:32:58 +0100

    fix linters errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,7 +7,9 @@ end
 
 RSpec.describe Post, type: :model do
   subject do
-    Post.new(title: 'my first post', text: 'this the first description text of my first descrption post', likes_counter: 0,
+    Post.new(title: 'my first post',
+             text: 'this the first description text of my first descrption post',
+             likes_counter: 0,
              comments_counter: 0)
   end
   before { subject.save }
