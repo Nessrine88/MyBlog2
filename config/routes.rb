1f3fdb4873a74144d35890b7971fd5b13bb5b12d
unknown <macherki.nessrine@yahoo.com>
2024-01-15 20:21:34 +0100

    add controller, routes and placeholder for the views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get '/users', to: 'users#index'
+  # get '/users/:id', to: 'users#show'
+  # get '/users/:author_id/posts', to: 'posts#index'
+  # get '/users/:author_id/posts/:id', to: 'posts#show'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
+  # Rest of your routes...
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
