ae23d857a79a06d8be48b403192ca20891d2b1c5
unknown <macherki.nessrine@yahoo.com>
2024-01-15 20:58:39 +0100

    fix the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 Rails.application.routes.draw do
-  # get '/users', to: 'users#index'
-  # get '/users/:id', to: 'users#show'
-  # get '/users/:author_id/posts', to: 'posts#index'
-  # get '/users/:author_id/posts/:id', to: 'posts#show'
 
-  resources :users, only: [:index, :show] do
+  root 'users#index'
+  resources :users, only: [:show] do
     resources :posts, only: [:index, :show]
   end
 
-  # Rest of your routes...
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
