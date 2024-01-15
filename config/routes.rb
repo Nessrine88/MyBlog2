fe50ef2d00bcba6bde7a1ec287226aa6e32594ca
unknown <macherki.nessrine@yahoo.com>
2024-01-15 21:05:43 +0100

    update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   root 'users#index'
-  resources :users, only: [:show] do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
