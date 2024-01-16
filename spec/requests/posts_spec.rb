9b2a3737ceee92d35a6b2db5289844d1318a5fef
unknown <macherki.nessrine@yahoo.com>
2024-01-16 15:45:18 +0100

    add controller spec files

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,15 +3,34 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/posts/index'
+      get '/users/:id/posts'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/posts/show'
+      get '/users/:id/posts/:id'
       expect(response).to have_http_status(:success)
     end
+  end
+
+  it 'renders the index template' do
+    get '/users/:id/posts'
+    expect(response).to render_template(:index)
+  end
+
+  it 'renders the show template' do
+    get '/users/:id/posts/:id'
+    expect(response).to render_template(:show)
+  end
+
+  it 'includes the correct placeholder text in the response body' do
+    get '/users/:id/posts'
+    expect(response.body).to include('Here is the list of posts for a given user')
+  end
+  it 'includes the correct placeholder text in the response body' do
+    get '/users/:id/posts/:id'
+    expect(response.body).to include('Here is a specific post for a given user')
   end
 end
