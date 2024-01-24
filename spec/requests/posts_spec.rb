eca034580745cff10eded8abdc38a753b50390c6
unknown <macherki.nessrine@yahoo.com>
2024-01-24 01:26:01 +0100

    fiw the tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,36 +1,40 @@
 require 'rails_helper'
+require 'factory_bot_rails'
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users/:id/posts'
+      @user = create(:user)
+      get user_posts_path(@user)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/:id/posts/:id'
+      @user = create(:user)
+      @post = create(:post)
+      get user_post_path(@user, @post)
       expect(response).to have_http_status(:success)
     end
   end
 
   it 'renders the index template' do
-    get '/users/:id/posts'
+    @user = create(:user)
+    get user_posts_path(@user)
     expect(response).to render_template(:index)
   end
 
   it 'renders the show template' do
-    get '/users/:id/posts/:id'
+    @user = create(:user)
+    @post = create(:post)
+    get user_post_path(@user, @post)
     expect(response).to render_template(:show)
   end
 
-  it 'includes the correct placeholder text in the response body' do
-    get '/users/:id/posts'
-    expect(response.body).to include('Here is the list of posts for a given user')
-  end
-  it 'includes the correct placeholder text in the response body' do
-    get '/users/:id/posts/:id'
-    expect(response.body).to include('Here is a specific post for a given user')
-  end
 end
